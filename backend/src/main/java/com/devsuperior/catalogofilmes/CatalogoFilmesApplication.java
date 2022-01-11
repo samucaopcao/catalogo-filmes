ee4b30fac474b01bb24e5d05bce881301893a47e
Samuel M dos Santos <samuelmacedo2011@gmail.com>
2022-01-11 00:29:16 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/catalogofilmes/CatalogoFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.catalogofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoFilmesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/catalogofilmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.catalogofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
