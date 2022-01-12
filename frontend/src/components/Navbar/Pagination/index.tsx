3633a738373f3ea4c18fdeda88d211657f75f094
Samuel M dos Santos <samuelmacedo2011@gmail.com>
2022-01-11 23:51:51 -0300

    Pagination Layout

---
+++ frontend/src/components/Navbar/Pagination/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as Arrow} from 'assets/img/arrow.svg'
+import './styles.css'
+
+function Pagination() {
+    return (
+        <div className="catalogo-filmes-pagination-container">
+            <div className="catalogo-filmes-pagination-box">
+                <button className="catalogo-filmes-pagination-button" disabled={true} >
+                    <Arrow />
+                </button>
+                <p>{`${1} de ${3}`}</p>
+                <button className="catalogo-filmes-pagination-button" disabled={false} >
+                    <Arrow className="catalogo-filmes-flip-horizontal" />
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Pagination;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,8 @@
+import Pagination from "components/Navbar/Pagination";
+
 function Listing() {
     return (
-        <h1>Página de Listagem</h1>
+        <Pagination/>
     );
 }
 
