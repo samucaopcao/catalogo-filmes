cb399b3ce253a4091b54bd03a73a76af1a7f31e6
Samuel M dos Santos <samuelmacedo2011@gmail.com>
2022-01-15 21:53:30 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,19 +2,30 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
-    //FORMA ERRADA 
+    const [pageNumber, setPageNumber] = useState(0);
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, []);
+
+
+
+
 
 
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
