79d7a18de42f65aa7177e0d3e5fb84c7e895a42f
Samuel M dos Santos <samuelmacedo2011@gmail.com>
2022-01-16 22:00:56 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,15 +1,25 @@
-import {ReactComponent as Arrow} from 'assets/img/arrow.svg'
+import { ReactComponent as Arrow } from 'assets/img/arrow.svg'
+import { MoviePage } from 'types/movie';
 import './styles.css'
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
 
-function Pagination() {
+function Pagination({ page, onChange }: Props) {
+
+
+
     return (
         <div className="catalogo-filmes-pagination-container">
             <div className="catalogo-filmes-pagination-box">
-                <button className="catalogo-filmes-pagination-button" disabled={true} >
+                <button className="catalogo-filmes-pagination-button" disabled={page.first}
+                    onClick={() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="catalogo-filmes-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="catalogo-filmes-pagination-button" disabled={page.last}
+                    onClick={() => onChange(page.number + 1)} >
                     <Arrow className="catalogo-filmes-flip-horizontal" />
                 </button>
             </div>
